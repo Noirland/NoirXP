62555e53149321f6dcb60f90aac5a697deb0de1b
LiborB <libor.bicanovsky@gmail.com>
2018-11-07 15:25:57 +1300

    Implemented levelToUse for using items with left click

--- src/main/java/NoirLeveling/Events/PlayerEvents.java
+++ src/main/java/NoirLeveling/Events/PlayerEvents.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -76,6 +77,9 @@ public class PlayerEvents implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (!event.hasItem()) {
+            return;
+        }
+        if (event.getAction() != Action.LEFT_CLICK_AIR || event.getAction() != Action.LEFT_CLICK_BLOCK) {
             return;
         }
 
