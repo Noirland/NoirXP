72075054ee1f6d3ec37418a89233de4f9f1de88b
ZephireNZ <brynleymcdonald@gmail.com>
2018-11-25 13:35:44 +1300

    Fix enchanting NPE errors

--- src/main/java/nz/co/noirland/noirxp/events/EnchantEvents.java
+++ src/main/java/nz/co/noirland/noirxp/events/EnchantEvents.java
@@ -2,8 +2,10 @@ package nz.co.noirland.noirxp.events;
 
 import nz.co.noirland.noirxp.callbacks.PlayerCallbacks;
 import nz.co.noirland.noirxp.classes.NoirPlayer;
+import nz.co.noirland.noirxp.config.UserdataConfig;
 import nz.co.noirland.noirxp.constants.PlayerClass;
 import nz.co.noirland.noirxp.NoirXP;
+import org.bukkit.enchantments.EnchantmentOffer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
@@ -15,36 +17,23 @@ public class EnchantEvents implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onPrepareEnchant(PrepareItemEnchantEvent event) {
-        NoirPlayer noirPlayer = NoirXP.players.get(event.getEnchanter().getUniqueId().toString());
-        if (noirPlayer.alchemy.getLevel() < 20) {
-            event.setCancelled(true);
-            return;
-        }
-        if (noirPlayer.alchemy.getLevel() < 30) {
-            if (event.getOffers()[0].getEnchantmentLevel() > 1) {
-                event.getOffers()[0] = null;
-            }
-            if (event.getOffers()[1].getEnchantmentLevel() > 1) {
-                event.getOffers()[1] = null;
-            }
-            if (event.getOffers()[2].getEnchantmentLevel() > 1) {
-                event.getOffers()[2] = null;
-            }
-            return;
-        }
-        if (noirPlayer.alchemy.getLevel() < 40) {
-            if (event.getOffers()[0].getEnchantmentLevel() > 2) {
-                event.getOffers()[0] = null;
-            }
-            if (event.getOffers()[1].getEnchantmentLevel() > 2) {
-                event.getOffers()[1] = null;
-            }
-            if (event.getOffers()[2].getEnchantmentLevel() > 2) {
-                event.getOffers()[2] = null;
-            }
-            return;
-        }
+        if(!UserdataConfig.inst().isLevelling(event.getEnchanter().getUniqueId())) return;
 
+        NoirPlayer noirPlayer = NoirXP.players.get(event.getEnchanter().getUniqueId().toString());
+
+        int alchemyLevel = noirPlayer.alchemy.getLevel();
+        int maxEnchant = 0;
+        if(alchemyLevel > 20) maxEnchant = 1;
+        if(alchemyLevel > 30) maxEnchant = 2;
+        if(alchemyLevel > 40) maxEnchant = 3;
+
+        for(int i = 0; i < event.getOffers().length; i++) {
+            EnchantmentOffer offer = event.getOffers()[i];
+            if(offer == null) continue;
+            if(offer.getEnchantmentLevel() > maxEnchant) event.getOffers()[i] = null;
+
+
+        }
     }
 
     @EventHandler(ignoreCancelled = true)
